2a8e2c45c3fbdfc916245d4bf01559198fdc1b46
LordComfy <comfyseven4@gmail.com>
2019-04-12 17:14:32 -0400

    Add project files.

---
+++ WindowsFileExplorer/Form1.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer), CType(CType(65, Byte), Integer))
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.ForeColor = System.Drawing.Color.White
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(886, 85)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(798, 11)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TreeView1.BackColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.TreeView1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TreeView1.ForeColor = System.Drawing.Color.White
+        Me.TreeView1.LineColor = System.Drawing.Color.White
+        Me.TreeView1.Location = New System.Drawing.Point(0, 91)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(886, 528)
+        Me.TreeView1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer), CType(CType(45, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(886, 617)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.Panel1)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Name = "Form1"
+        Me.Text = "File Explorer"
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents TreeView1 As TreeView
+    Private WithEvents Button1 As Button
+End Class
---
+++ WindowsFileExplorer/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadRootDrives()
+    End Sub
+
+    Public Sub LoadRootDrives()
+        For Each drive As DriveInfo In DriveInfo.GetDrives
+            Dim node As New TreeNode(drive.Name)
+            With node
+                .Tag = drive.Name
+                .ImageKey = drive.Name
+                .Nodes.Add("Empty")
+            End With
+
+            TreeView1.Nodes.Add(node)
+        Next
+    End Sub
+
+    Public Sub LoadChildren(nd As TreeNode, dir As String)
+        Dim DirectoryInformation As New DirectoryInfo(dir)
+
+        Try
+            For Each d As DirectoryInfo In DirectoryInformation.GetDirectories
+
+                If Not (d.Attributes And FileAttributes.Hidden) = FileAttributes.Hidden Then
+                    Dim folder As New TreeNode(d.Name)
+
+                    With folder
+                        .Tag = d.FullName
+                        .ImageKey = d.FullName
+                        .Nodes.Add("*Empty*")
+                    End With
+
+                    nd.Nodes.Add(folder)
+                End If
+
+            Next
+        Catch ex As Exception
+            MessageBox.Show(ex.ToString())
+        End Try
+    End Sub
+
+    Private Sub TreeView1_BeforeExpand(sender As Object, e As TreeViewCancelEventArgs) Handles TreeView1.BeforeExpand
+        Dim IsDriveReady As Boolean = (From d As DriveInfo In DriveInfo.GetDrives Where d.Name = e.Node.ImageKey Select d.IsReady).FirstOrDefault()
+
+        If (e.Node.ImageKey <> "Desktop" AndAlso Not e.Node.ImageKey.Contains(":\")) OrElse IsDriveReady OrElse Directory.Exists(e.Node.ImageKey) Then
+            e.Node.Nodes.Clear()
+            LoadChildren(e.Node, e.Node.ImageKey.ToString)
+
+        ElseIf e.Node.ImageKey = "Dekstop" Then
+            e.Node.Nodes.Clear()
+
+            Dim DesktopFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.CommonDesktopDirectory)
+            Dim UserDesktopFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+
+            LoadChildren(e.Node, UserDesktopFolder)
+            LoadChildren(e.Node, DesktopFolder)
+        Else
+            e.Cancel = True
+            MessageBox.Show("Error drive is empty, " + e.Node.ImageKey.ToString())
+        End If
+
+    End Sub
+End Class
